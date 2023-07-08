1dcb844a11e0e1a9f7554677a6ead3a125db63db
unknown <ericksemplou@gmail.com>
2023-07-08 09:03:04 +0300

    add js code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 /* BURGER MENU */
+
 // Отримуємо посилання на кнопку
 let menu = document.getElementById("nav__toggle");
 
@@ -23,3 +24,67 @@ menu.addEventListener("click", function() {
     isActive = true;
   }
 });
+
+
+/* FIXED HEADER */
+
+window.addEventListener('scroll', function() {
+  let header = document.querySelector('.header'); // Вибираємо заголовок (header) сайту
+  let scrollPosition = window.scrollY; // Отримуємо позицію прокрутки
+  let windowHeight = window.innerHeight; // Отримуємо висоту вікна браузера
+
+  if (scrollPosition > windowHeight) {
+    header.classList.add('fixed'); // Додаємо клас "fixed" до заголовку, щоб його зафіксувати
+  } else {
+    header.classList.remove('fixed'); // Видаляємо клас "fixed" з заголовку, щоб його розфіксувати
+  }
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  let header = document.querySelector('.header');
+  let navToggle = document.querySelector('.nav__toggle');
+
+  // Функція для перевірки стану класу `fixed` у елемента `.header`
+  function checkHeaderFixed() {
+    if (header.classList.contains('fixed')) {
+      navToggle.style.marginRight = '20px'; 
+        
+    } else {
+      navToggle.style.marginRight = ''; 
+    }
+  }
+
+  // Викликати функцію перевірки при завантаженні сторінки
+  checkHeaderFixed();
+
+  // Викликати функцію перевірки при прокрутці сторінки
+  window.addEventListener('scroll', checkHeaderFixed);
+});
+
+
+/* SCROLL NAVIGATION */
+
+document.addEventListener('DOMContentLoaded', function() {
+    let links = document.querySelectorAll('a[data-scroll]');
+
+    links.forEach(function(link) {
+      link.addEventListener('click', function(e) {
+        e.preventDefault();
+        let targetId = link.getAttribute('data-scroll');
+        let targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+          targetSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    });
+});
+
+
+
+
+
+
+
+
